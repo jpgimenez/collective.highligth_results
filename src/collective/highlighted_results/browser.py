b7e829d0dcfe9b5ffc232ebb76ea57e0f8186ffd
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-02-04 12:12:21 -0300

    RD view and title viewlet.

--- src/collective/highlighted_results/browser.py
+++ src/collective/highlighted_results/browser.py
@@ -13,8 +13,11 @@ except:
     quote_chars = backward_search.quote_chars
     EVER = backward_search.EVER
 
+from zope.component import getMultiAdapter
+from zope.publisher.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-
+from collective.highlighted_results.content import Ird
+from plone.app.layout.viewlets.common import TitleViewlet as TitleViewletBase
 
 class Search(BaseSearch):
     """
@@ -38,3 +41,31 @@ class Search(BaseSearch):
         catalog = getToolByName(self.context, 'portal_catalog')
         return [i for i in catalog.queryCatalog(query, show_all=1, show_inactive=1)
                 if not i.inactive]
+
+
+class View(BrowserView):
+    """
+    """
+
+    def title(self):
+        context = self.context
+        return context.title and context.title or \
+               (context.target and context.target.to_object.Title() or ' ')
+
+    def link(self):
+        context = self.context
+        return context.link and context.link or \
+               (context.target and context.target.to_object.absolute_url() or ' ')
+
+
+class TitleViewlet(TitleViewletBase):
+
+    def update(self):
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.context_state = getMultiAdapter((self.context, self.request),
+                                             name=u'plone_context_state')
+        context = self.context
+        self.page_title = lambda: context.title and context.title or \
+            (context.target and context.target.to_object.Title() or ' ')
+        self.portal_title = self.portal_state.portal_title
